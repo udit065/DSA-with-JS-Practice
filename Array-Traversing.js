436d52fc24ec07d0b8ee8db89eea37d01a899991
Udit Yadav <udityadav133@gmail.com>
2023-09-09 19:08:20 +0530

    Done

---
+++ Array-Traversing.js
@@ -0,0 +1,13 @@
+let Arr = [1, 3, 6, 61, 545, 545, 66, 6];
+
+for (let i = 0; i < Arr.length; i++) {
+    // console.log(Arr[i]);
+}
+
+// Revere Traversal
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+for(let i= arr.length - 1; i >= 0; i--) {
+console.log(arr[i] + ' Reverse Traversal');
+}
